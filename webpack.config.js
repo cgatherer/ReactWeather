6c5acd4c408a2f43212d3da2bd335e6a0f0cbefb
Christopher Gatherer <box@christophersmbp.hibbertnet.com>
2017-10-11 16:16:42 -0400

    Added Link component

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Weather: 'app/components/Weather.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
