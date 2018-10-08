7c33020c4a5f2b05ab9eec2e7cba07084fe13dfe
Christopher Gatherer <chris.gatherer@gmail.com>
2018-10-08 18:11:59 -0400

    Error Modal

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,36 +3,44 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
   onSearch: function (e) {
-    e.preventDefault();
-    alert('Not yet wired up!');
+      e.preventDefault();
+
+      var location = this.refs.search.value;
+      var encodedLocation = encodeURIComponent(location);
+
+      if (location.length > 0) {
+        this.refs.search.value = '';
+        window.location.hash = '#/?location=' + encodedLocation;
+      }
   },
   render: function () {
     return (
-      <div>
-        <div className="top-bar" id="main-menu">
-          <div className="top-bar-left">
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
             <ul className="menu">
-              <li className="menu-text">React Weather App</li>
-              <li>
-                <IndexLink to="/" activeclassNameName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              <li className="inline">
+                <input type="search" placeholder="Search weather by city" ref="search"/>
               </li>
-              <li>
-                <Link to="/about" activeclassNameName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
-              </li>
-              <li>
-                <Link to="/examples" activeclassNameName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+              <li className="inline">
+                <input type="submit" className="button" value="Get Weather"/>
               </li>
             </ul>
-          </div>
-
-          <div className="top-bar-right">
-            <ul className="menu">
-              <form onSubmit={this.onSearch}>
-                <li className="inline"><input type="search" placeholder="Search weather by city"/></li>
-                <li className="inline"><input type="submit" className="button" value="Get Weather"/></li>
-              </form>
-            </ul>
-          </div>
+          </form>
         </div>
       </div>
     );
