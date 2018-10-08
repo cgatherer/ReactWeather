1f1813ced030e6a21b82eea47a91f46494f986c9
Christopher Gatherer <chris.gatherer@gmail.com>
2018-10-08 17:13:37 -0400

    Added Sass

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -13,6 +13,9 @@ var About = (props) => {
           <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the Javascript framework used.
         </li>
         <li>
+          <a href="https://github.com/cgatherer/ReactWeather" target="_blank">Github</a> - This the github repository where my project lives.
+        </li>
+        <li>
           <a href="https://openweathermap.org" target="_blank">Open Weather Map</a> - I used Open Weather Map to search for weather data by weather name.
         </li>
       </ul>
